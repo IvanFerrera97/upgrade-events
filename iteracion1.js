56717b7bcc09d38ade5969049aa5f363e752ab70
Ivan <ivanferrera67@gmail.com>
2022-05-09 02:55:10 +0200

    Ejercicios events

---
+++ iteracion1.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+
+//1.1
+    let button = document.querySelector(".click")
+    button.setAttribute("id", "btnToClick");
+    button.setAttribute("type", "button");
+  
+    
+    button.addEventListener("click", function(event){
+        console.log(event);
+       
+    })
+
+
+//1.2
+
+ document.querySelector(".focus").addEventListener("focus", function(event){
+       console.log(event.target.value);
+      
+ })
+
+
+
+
+ document.querySelector(".value").addEventListener("input", function(event){
+    console.log(event.target.value);
+   
+})
+
+}
+
